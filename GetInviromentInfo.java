2c6706757eaf0629a5ffbb958dc74d9ecf110004
Waseem Besada <waseem@telia.com>
2015-12-29 00:01:32 +0100

    Get enviroment information update3

--- GetInviromentInfo.java
+++ GetInviromentInfo.java
@@ -10,5 +10,6 @@ public static void main (String[] args){
 	System.out.println("dataModel 	= "+dataModel);
 	System.out.println("vmVersion 	= "+vmVersion);
 	System.out.println("osArch    	= "+osArch);
+	System.out.println("========================")
 	}
 }
