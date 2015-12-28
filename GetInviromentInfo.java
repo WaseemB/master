1ec21324dc71649c72ad51331e73e0eab42cfe50
Waseem Besada <waseem@telia.com>
2015-12-28 23:12:10 +0100

    Get enviroment information update

--- GetInviromentInfo.java
+++ GetInviromentInfo.java
@@ -1,14 +1,14 @@
 
 public class GetInviromentInfo {
 	private static final String osName		=	System.getProperty("os.name");
-	private static final String dataModel 	=	System.getProperty("sun.arch.data.model");
-	private static final String vmVersion 	=	System.getProperty("java.vm.version");
+	private static final String dataModel	=	System.getProperty("sun.arch.data.model");
+	private static final String vmVersion	=	System.getProperty("java.vm.version");
 	private static final String osArch		= 	System.getProperty("os.arch");
 
 public static void main (String[] args){
-	System.out.println("osName = "+osName);
-	System.out.println("dataModel = "+dataModel);
-	System.out.println("vmVersion = "+vmVersion);
-	System.out.println("osArch    = "+osArch);
+	System.out.println("osName	= "+osName);
+	System.out.println("dataModel 	= "+dataModel);
+	System.out.println("vmVersion 	= "+vmVersion);
+	System.out.println("osArch    	= "+osArch);
 	}
 }
